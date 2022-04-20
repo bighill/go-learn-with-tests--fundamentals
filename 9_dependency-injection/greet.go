671f482ad5d627a4f68ed04f2991f225cde0d6c3
lex <alex@bighill.org>
2022-04-19 23:36:39 -0700

    Import dependencyinjection package in main to serve greeter on http.

--- 9_dependency-injection/greet.go
+++ 9_dependency-injection/greet.go
@@ -7,21 +7,18 @@ import (
 	"os"
 )
 
-func GreetToStdOut() {
-	Greet(os.Stdout, "Alexander")
-}
-
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func GreetToStdOut() {
+	Greet(os.Stdout, "Alexander")
 }
 
 func GreeterHandler(w http.ResponseWriter, r *http.Request) {
 	Greet(w, "world")
 }
 
-/*
-	This would work if this were package main
-*/
-// func main() {
-// 	http.ListenAndServe(":5000", http.HandlerFunc(GreeterHandler))
-// }
+func ServeGreeter() {
+	http.ListenAndServe(":5000", http.HandlerFunc(GreeterHandler))
+}
--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	dependencyinjection "learn-go-with-tests/9_dependency-injection"
+)
 
 func main() {
 	fmt.Println("This is main.")
 	fmt.Println("It's not connected to all the child packages because the meat of this is in running the test within each package.")
 	fmt.Println("Try running this command:")
 	fmt.Println("    go test ./...")
+
+	dependencyinjection.ServeGreeter()
 }
